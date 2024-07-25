5de57ba96cc737cab89a820e8f43a647fbbe2d89
EzeZari <Ezequielzari@hotmail.com>
2024-07-25 14:29:02 -0300

    ADD - Temporizador

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var btnIngresar = document.getElementById("btnIngresar");
 var inputNombre = document.getElementById("nombreJugador").value;
 var juego = document.getElementById("juegoBoogle");
 var modalInicio = document.getElementById("modalInicio");
+var temporizador = document.getElementById("temporizador")
 
 function validarNombre() {
     var inputNombre = document.getElementById("nombreJugador").value;
@@ -27,9 +28,10 @@ function abrirJuego() {
     modalInicio.style.display = "none";
     juego.style.display = "block";
     asignarLetrasAleatorias();
+    iniciarTemporizador(180); //Cambiar a tiempoSeleccionado
 }
 
-btnIngresar.addEventListener("click", recibirNombre)
+btnIngresar.addEventListener("click", recibirNombre) //CAMBIAR A ABRIRJUEGO
 
 function asignarLetrasAleatorias() {
     const letras = "AAABCDEEEFGHIIIJKLMNOOOPQRSTUUUVWXYZ";
@@ -39,4 +41,27 @@ function asignarLetrasAleatorias() {
         const letraAleatoria = letras[Math.floor(Math.random() * letras.length)];
         boton.textContent = letraAleatoria;
     });
+}
+
+function iniciarTemporizador(duracion) {
+    var tiempoRestante = duracion;
+    var intervalo = setInterval(() => {
+        var minutos = Math.floor(tiempoRestante / 60);
+        var segundos = tiempoRestante % 60;
+
+        temporizador.textContent =
+            (minutos < 10 ? "0" + minutos : minutos) + ":" +
+            (segundos < 10 ? "0" + segundos : segundos);
+
+        if (tiempoRestante <= 10){
+            temporizador.style.color="red"
+        }
+        if (tiempoRestante <= 0) {
+            clearInterval(intervalo);
+            temporizador.textContent = "00:00";
+            // Agregar acción cuando el temporizador llegue a cero
+        } else {
+            tiempoRestante--;
+        }
+    }, 1000);
 }
