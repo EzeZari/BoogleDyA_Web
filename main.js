72fe78b3f7e022e96c37dbc7e0265a1a130103c6
EzeZari <Ezequielzari@hotmail.com>
2024-07-27 17:58:42 -0300

    ADD - Funcion para seleccionar tiempo de juego.

--- main.js
+++ main.js
@@ -2,41 +2,51 @@ var btnIngresar = document.getElementById("btnIngresar");
 var inputNombre = document.getElementById("nombreJugador").value;
 var juego = document.getElementById("juegoBoogle");
 var modalInicio = document.getElementById("modalInicio");
-var temporizador = document.getElementById("temporizador")
+var temporizador = document.getElementById("temporizador");
+var eleccionTiempo = document.getElementById("tiempoJuego").value;
+var tiempodeJuego = 0;
 
 function validarNombre() {
     var inputNombre = document.getElementById("nombreJugador").value;
-
-    if (inputNombre.length >= 3) {
+    
+    if (inputNombre.length >= 3 ) {
         document.getElementById("mensaje").textContent = "";
-        btnIngresar.disabled = false;
     } else {
         document.getElementById("mensaje").textContent = "El nombre debe tener al menos 3 caracteres.";
-        document.getElementById("mensaje").style.color = "red";
+    }
+    validarBoton();
+}
+function validarBoton() {
+    var inputNombre = document.getElementById("nombreJugador").value;
+    var eleccionTiempo = document.getElementById("tiempoJuego").value;
+    if (inputNombre.length >= 3 && eleccionTiempo !== "") {
+        btnIngresar.disabled = false;
+    } else {
         btnIngresar.disabled = true;
     }
 }
+
 
 function recibirNombre() {
     var inputNombre = document.getElementById("nombreJugador").value;
     var saludoJugador = document.getElementById("saludoJugador")
     saludoJugador.innerHTML = `¡A jugar ${inputNombre}!`
-    abrirJuego();
 }
 
 function abrirJuego() {
     modalInicio.style.display = "none";
     juego.style.display = "block";
+    recibirNombre();
     asignarLetrasAleatorias();
-    iniciarTemporizador(180); //Cambiar a tiempoSeleccionado
+    iniciarTemporizador(tiempodeJuego); 
 }
 
-btnIngresar.addEventListener("click", recibirNombre) //CAMBIAR A ABRIRJUEGO
+btnIngresar.addEventListener("click", abrirJuego) 
 
 function asignarLetrasAleatorias() {
     const letras = "AAABCDEEEFGHIIIJKLMNOOOPQRSTUUUVWXYZ";
     const botones = document.querySelectorAll(".gridBoogle .item button");
-
+    
     botones.forEach(boton => {
         const letraAleatoria = letras[Math.floor(Math.random() * letras.length)];
         boton.textContent = letraAleatoria;
@@ -48,13 +58,13 @@ function iniciarTemporizador(duracion) {
     var intervalo = setInterval(() => {
         var minutos = Math.floor(tiempoRestante / 60);
         var segundos = tiempoRestante % 60;
-
+        
         temporizador.textContent =
-            (minutos < 10 ? "0" + minutos : minutos) + ":" +
-            (segundos < 10 ? "0" + segundos : segundos);
-
-        if (tiempoRestante <= 10){
-            temporizador.style.color="red"
+        (minutos < 10 ? "0" + minutos : minutos) + ":" +
+        (segundos < 10 ? "0" + segundos : segundos);
+        
+        if (tiempoRestante <= 10) {
+            temporizador.style.color = "red" //Ponerlo en CSS
         }
         if (tiempoRestante <= 0) {
             clearInterval(intervalo);
@@ -64,4 +74,18 @@ function iniciarTemporizador(duracion) {
             tiempoRestante--;
         }
     }, 1000);
+}
+
+
+function eleccionTiempoJuego() {
+    eleccionTiempo = document.getElementById("tiempoJuego").value;
+    
+    if (eleccionTiempo == "1") {
+        tiempodeJuego = 60;
+    } else if (eleccionTiempo == "2") {
+        tiempodeJuego = 120;
+    } else if (eleccionTiempo == "3") {
+        tiempodeJuego = 180;
+    } 
+    validarBoton();
 }
